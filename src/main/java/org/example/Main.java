a0b8289c7261cf345911a68c8034f23489c5134d
Ethan Irvin <aethil@yahoo.com>
2023-03-30 20:01:27 -0500

    Studio Complete

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] charactersInString = hiddenFigures.toLowerCase().replaceAll("/[^a-z]+/ig", "").toCharArray();
+
+        HashMap<Character, Integer> name = new HashMap<>();
+
+        for(char i: charactersInString){
+            if(name.containsKey(i)) {
+              name.put(i,name.get(i) +1);
+            } else {
+              name.put(i, 1);
+            }
+        }
+
+
+        System.out.println(name);
+    }
+}
